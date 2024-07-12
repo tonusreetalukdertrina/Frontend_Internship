9635328dd1335ab94a89e05468886748c1dbcd24
Tonusree Talukder Trina <85805255+tonusreetalukdertrina@users.noreply.github.com>
2024-07-12 14:36:22 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Form from './Form';
 import Data from './Data';
-import Header from './Header';  // Import the Header component
+import Header from './Header';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />  {/* Add the Header component */}
+      <Header />
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/data" element={<Data />} />
